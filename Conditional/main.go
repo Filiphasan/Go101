d05c03b207ee10cd0873f240e202cc2c61557245
Hasan Erdal <hasaerda@hotmail.com>
2024-07-21 16:15:58 +0300

    Switch statement add in conditional

--- Conditional/main.go
+++ Conditional/main.go
@@ -21,4 +21,34 @@ func main() {
 	} else {
 		fmt.Println("Your number is zero")
 	}
+
+	// switch statement
+	switch number {
+	case 1:
+		fmt.Println("Your number is 1")
+	case 2:
+		fmt.Println("Your number is 2")
+	case 3:
+		fmt.Println("Your number is 3")
+	default:
+		fmt.Println("Your number is not 1, 2, 3")
+	}
+
+	// switch with multi case
+	switch number {
+	case 1, 2, 3:
+		fmt.Println("Your number is one of 1, 2, 3")
+	default:
+		fmt.Println("Your number is not one of 1, 2, 3")
+	}
+
+	// switch with condition
+	switch {
+	case number > 0:
+		fmt.Println("Your number is positive")
+	case number < 0:
+		fmt.Println("Your number is negative")
+	default:
+		fmt.Println("Your number is zero")
+	}
 }
